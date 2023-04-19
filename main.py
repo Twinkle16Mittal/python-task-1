255c4137cde561a80229338b7bd35752c054b26f
Twinkle Mittal <twinklemittal@Twinkles-MacBook-Air.local>
2023-04-19 22:44:49 +0530

    merge two list

--- main.py
+++ main.py
@@ -26,7 +26,29 @@ def merge_lists(list_1, list_2) -> list:
     - Both lists are unsorted
     - Both lists can have missing values (for ex list_2 has missing id=2)
     """
-    # return list_3
+    
+    list_1.extend(list_2) 
+    list_1 = sorted(list_1, key=lambda i: i['id'])  # Sorting based on id
+
+    len1 = len(list_1)
+    list_3 = []
+    i=0
+    while i<len1-1:
+        if list_1[i]['id'] == list_1[i+1]['id']: 
+            res = list_1[i]
+            res.update(list_1[i+1])
+            list_3.append(res)
+            i+=2
+        else:
+            list_3.append(list_1[i])
+            i+=1
+    if list_1[len1-2]['id'] != list_1[len1-1]['id'] :
+        # if the last element is not equal to previous one then append
+        list3.append(list_1[len1-1])
+    
+    return list_3
+    
 
 
 list_3 = merge_lists(list_1, list_2)
+print(list_3)
